79395036b45045db845ed023d90197fbede1bbaf
TomasMesaPierotti <92758600+TomasMesaPierotti@users.noreply.github.com>
2021-10-18 18:04:42 -0300

    Add files via upload

---
+++ js/recetario.js
@@ -0,0 +1,9 @@
+
+
+var ingrediente1 = prompt("Introdusca el primer ingrediente: ", "")
+var ingrediente2 = prompt("Introdusca el segundo ingrediente: ", "")
+var ingrediente3 = prompt("Introdusca el tercer ingrediente: ", "")
+
+document.write("<h2><u>lista de Ingredientes</u></h2>")
+
+document.write( "<ul>" + "<li>" + ingrediente1 + "</li>" + "<li>"+ ingrediente2 + "</li>" +" <li>" + ingrediente3 + "</li>" + "</ul>");
